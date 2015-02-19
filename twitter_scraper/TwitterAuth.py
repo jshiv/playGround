36a0c6a2f126ccaec78380c2016aad986c5aa7b8
Jason Shiverick <jshiverick@lus-jshiverick3.teslamotors.com>
2015-02-19 10:23:10 -0800

    added twitter scrape code

---
+++ twitter_scraper/TwitterAuth.py
@@ -0,0 +1,10 @@
+# Go to http://dev.twitter.com and create an app. 
+# The consumer key and secret will be generated for you after
+consumer_key = 'wGEa1Dw1OsAAYZHUvpsJZtlSd'
+consumer_secret = 'MC3rcfppNqI2Q4AxnMbwGlLSn5ovNKM9ZMk1WKLYbozbDNIgm4'
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+
+access_token = '50725053-wEMMWyblLnUsCKmIXEkWUnBtXjUxEpBhAN5ayMal2'
+access_token_secret = 'geJpbyeyffbHXg01UQVBrz07iGdAZfRCkPcRWFXIOZSEE'
