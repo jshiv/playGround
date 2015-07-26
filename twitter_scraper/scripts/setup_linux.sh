ec3b7fcf1cfc020573ae4664a4256cb9ab2aa2e8
jshiv <jshiv00@gmail.com>
2015-07-25 21:41:09 -0700

    added linux scripts

---
+++ twitter_scraper/scripts/setup_linux.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-pip
+sudo apt-get install python-dev
+
+pip install json
+pip install tweepy
---
+++ twitter_scraper/scripts/ssh_aws.sh
@@ -0,0 +1,1 @@
+ssh -i ~/.aws/keypair.pem ubuntu@ec2-52-2-185-108.compute-1.amazonaws.com
